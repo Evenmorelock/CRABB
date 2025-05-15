5235d8ba6076a751325a22fe95bd4037ee00e294
Evenmorelock <jakepinkerton123456789@gmail.com>
2025-05-15 12:15:54 +0100

    AA

--- Assets/hatUnlock.cs
+++ Assets/hatUnlock.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 
 public class hatUnlock : MonoBehaviour
 {
-    public Collectable chefHat;
-    public Collectable cone;
-    public Collectable box;
-    public Collectable bucket;
-    public Collectable standardCap;
-    public Collectable spikyWig;
-    public Collectable skull;
-    public Collectable helmet;
-    public Collectable securityCap;
-    public Collectable propellerCap;
-    public Collectable plate;
+ 
 
     // Start is called before the first frame update
     void Start()
