edf8538426cad70fb2545071f1aeba0788a9ee21
Evenmorelock <jakepinkerton123456789@gmail.com>
2025-05-21 12:21:43 +0100

    pause menu done, working on cosmetics

--- Assets/pauseMenu.cs
+++ Assets/pauseMenu.cs
@@ -3,11 +3,38 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
+using UnityEngine.SceneManagement;
 
 public class pauseMenu : MonoBehaviour
 {
     public static bool gameIsPaused = false;
     public GameObject pauseMenuUI;
+    public GameObject HatMenuUI;
+
+    public bool skullUnlocked;
+    public bool bucketUnlocked;
+    public bool helmetUnlocked;
+    public bool chefHatUnlocked;
+    public bool propellerCapUnlocked;
+    public bool boxUnlocked;
+    public bool coneUnlocked;
+    public bool redCapUnlocked;
+    public bool wigUnlocked;
+    public bool securityCapUnlocked;
+    public bool plateUnlocked;
+
+    public GameObject skull;
+    public GameObject bucket;
+    public GameObject helmet;
+    public GameObject chefHat;
+    public GameObject propellerCap;
+    public GameObject box;
+    public GameObject cone;
+    public GameObject redCap;
+    public GameObject wig;
+    public GameObject securityCap;
+    public GameObject plate;
+
 
     // Start is called before the first frame update
     void Start()
@@ -32,14 +59,33 @@ public class pauseMenu : MonoBehaviour
     }
     public void Resume()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         gameIsPaused = false;
     }
     public void Pause()
     {
+        Cursor.lockState = CursorLockMode.None;
        pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         gameIsPaused = true;
     }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Time.timeScale = 1f;
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+    public void OpenHats()
+    {
+        Debug.Log("Uh Oh it hat time");
+        pauseMenuUI.SetActive(false);
+        HatMenuUI.SetActive(true);
+    }
+
 }
